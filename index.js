dba0fbbd760ee9fceab08075dc79856a356646ab
Evans Ochieng <evansochieng121@gmail.com>
2022-08-20 22:12:45 +0300

    Completed assignment/tried out new stuff

--- index.js
+++ index.js
@@ -20,16 +20,42 @@ function moveDodgerLeft() {
     }
   });
 
-  // Assignment
-  // Function 2 (moveDodgerRight())
+// Assignment
+// Function 2 (moveDodgerRight())
+
+// First I extract the widths of the game div and the dodger width
+// Try out something boy
+// Get the css styling properties
+const game = document.getElementById('game');
+
+const gameDivStyling = getComputedStyle(game); // This gives css styling for this object
+// Get width of that element (game div)
+let gameWidth = gameDivStyling.getPropertyValue('width');
+gameWidth = gameWidth.replace('px', '');
+gameWidth = parseInt(gameWidth.replace('px', ''), 10);
+
+// This works
+// With this I can modify the condition to check 'dodge' 
+//doesn't disappear
+
+// Remove hard-coding from moveDodgerRight() Function
+// Get the width of the dodger div
+const dodgerDivStyling = getComputedStyle(dodger);
+
+let dodgerWidth = dodgerDivStyling.getPropertyValue('width');
+dodgerWidth = dodgerWidth.replace('px', '');
+dodgerWidth = parseInt(dodgerWidth, 10);
+
+// Now the condition that I will be checking is:
+// if (left < (gameWidth - dodgerWidth))
 
   // Create the move dodger right function
   function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace('px', '');
     const left = parseInt(leftNumbers, 10);
 
-    if (left < 360 ){
-        dodger.style.left = `${left + 1}px`;
+    if (left < 360){ // Check if the doger is within the container
+        dodger.style.left = `${left + 1}px`; // Move the dodger right by adding 1 
     }
   }
 
@@ -42,17 +68,31 @@ function moveDodgerLeft() {
     }
   });
 
-  // Try out something boy
-  // Get the css styling properties
-  const game = document.getElementById('game');
+// // Try out something boy
+// // Get the css styling properties
+// const game = document.getElementById('game');
 
-  const styling = getComputedStyle(game);
-  // Get width of that element
-  let gameWidth = styling.getPropertyValue('width');
-  gameWidth = gameWidth.replace('px', '');
-  gameWidth = parseInt(gameWidth.replace('px', ''), 10);
+// const gameDivStyling = getComputedStyle(game); // This gives css styling for this object
+// // Get width of that element (game div)
+// let gameWidth = gameDivStyling.getPropertyValue('width');
+// gameWidth = gameWidth.replace('px', '');
+// gameWidth = parseInt(gameWidth.replace('px', ''), 10);
 
-  // With this I can modify the condition to check 'dodge' 
-  //doesn't disappear
+// // This works
+// // With this I can modify the condition to check 'dodge' 
+// //doesn't disappear
 
+// // Remove hard-coding from moveDodgerRight() Function
+// // Get the width of the dodger div
+// const dodgerDivStyling = getComputedStyle(dodger);
 
+// let dodgerWidth = dodgerDivStyling.getPropertyValue('width');
+// dodgerWidth = dodgerWidth.replace('px', '');
+// dodgerWidth = parseInt(dodgerWidth, 10);
+
+// // Now the condition that I will be checking is:
+// // if (left < (gameWidth - dodgerWidth))
+// I don't know why the test is failing though
+
+//Good Boy!
+// Always minimize hard-coding in your programs. Be flexible as much as possible man!
